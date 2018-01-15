e7b0123fc8c997bf9a5a1796333cfd8c1171ab09
aavshr <aavash09@gmail.com>
2018-01-15 03:09:12 +0100

    make multiple pipes possible

--- execute.c
+++ execute.c
@@ -10,9 +10,9 @@ static const char* progname = "shell";
 
 int execute(Command *c){
 	pid_t pid; 
-	int i,j,k,status;
+	int i, j, k, status, bkgd_num = 0;
 	int prog_num = c->pipeNum+1;
-	int first_child = 0, last_child = 0;
+	int first_child = 1, last_child = 0;
 	FILE *in, *out;
 	char buffer[256];
 
@@ -27,10 +27,10 @@ int execute(Command *c){
 		args[i][c->ComTable[0]->argnum+1] = (char*) NULL;
 	}
 
-	int fds[2*c->pipeNum], io[2]; 
+	int fds[c->pipeNum*2]; 
 	for(i =0; i <(c->pipeNum);i++){
 		if(pipe(fds + i*2) < 0){
-			perror("initial pipe");
+			perror(progname);
 			exit(1);
 		}
 	}
@@ -58,48 +58,53 @@ int execute(Command *c){
 
 			if(c->ComTable[i]->outfile){
 				close(1);
-				fopen(c->ComTable[i]->outFileName, "w");
-				if(open(c->ComTable[i]->outFileName, O_WRONLY|O_TRUNC) < 0){
+				if(open(c->ComTable[i]->outFileName, O_CREAT|O_WRONLY|O_TRUNC, 0600) < 0){
 					perror(progname);
 					exit(1);
 				}
 			}
 
 			if(!first_child && prog_num > 1){
-				if(dup2(fds[j-2],STDIN_FILENO) < 0){
+				//close(fds[i-1][1]);
+				if(dup2(fds[(j-1)*2],STDIN_FILENO) < 0){
 					perror("not first child");
 					exit(1);
 				}
+				//close(fds[i-1][0]);
 			}
 
 			if(!last_child && prog_num > 1){
-				if(dup2(fds[j+1],STDOUT_FILENO)<0){
+				//close(fds[i][0]);
+				if(dup2(fds[j*2+1],STDOUT_FILENO)<0){
 					perror("not last child");
 					exit(1);
-				}	
+				}
+				//close(fds[i][1]);	
 			}
 
-			for(k = 0; k < 2*(c->pipeNum); k++){
-                close(fds[k]);
-            }
+			
+			for(k=0;k<(2*c->pipeNum);k++){
+				close(fds[k]);
+			}
 
             if(c->ComTable[i]->background){
             	daemon(1,1);
+            	bkgd_num++;
             }
 			execvp(args[i][0],args[i]);
 			perror(args[i][0]);
 			exit(1);
 		}
-		j+=2;
-	}
-
-	for(k=0;k<2*(c->pipeNum);k++){
-			close(fds[k]);
-	}	
-
-	for(k=0;k<prog_num;k++){
-		wait(&status);
+		j++;
 	}
 		
+	for(k=0;k<(2*c->pipeNum);k++){
+		close(fds[k]);
+	}
+
+	for(k=0;k<(prog_num-bkgd_num);k++){
+			wait(&status);
+	}	
+	
 	return 1;
 }
